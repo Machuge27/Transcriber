e602ab7dbf6c70a2f600ade9138656ee0df2586b
Machuge27 <mutaihillary3912@gmail.com>
2025-03-30 17:04:52 +0300

    Initial deplyment

--- src/services/API.js
+++ src/services/API.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // https://hillarymutai.pythonanywhere.com/
 // https://transcriber-backend-l6tb.onrender.com/
 // const BASE_URL = 'https://transcriber-backend-l6tb.onrender.com/api/';
-// const BASE_URL = 'https://hillarymutai.pythonanywhere.com/api/';
-const BASE_URL = 'http://192.168.100.6:8000/api';
+const BASE_URL = 'https://hillarymutai.pythonanywhere.com/api/';
+// const BASE_URL = 'http://192.168.100.6:8000/api';
 // const BASE_URL = 'http://192.168.0.115:8000/api';
 
 // Create an axios instance with default configuratio
